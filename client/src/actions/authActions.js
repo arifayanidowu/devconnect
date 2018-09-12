d022e012abb6e8ed3847ae9c43d587a2b712c214
stizzle123 <arifayanidowu2@yahoo.com>
2018-09-12 09:14:05 +0100

    Add authReducer,registerUser,loginUser,login validation and error handling

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from "../utils/setAuthToken";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const loginUser = userData => dispatch => {
+  axios
+    .get("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to LS
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set Current User
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
