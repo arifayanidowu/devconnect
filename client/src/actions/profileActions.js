2fa7d83e1e4d82383ed7446d66852c14e0c90e3c
stizzle123 <arifayanidowu2@yahoo.com>
2018-09-17 00:17:47 +0100

    Create Profile Components and sub-components

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -28,6 +28,25 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
@@ -45,21 +64,20 @@ export const createProfile = (profileData, history) => dispatch => {
 export const getProfiles = () => dispatch => {
   dispatch(setProfileLoading());
   axios
-  .get('/api/profile/all')
-  .then(res => 
-    dispatch({
-      type: GET_PROFILES,
-      payload: res.data
-    })  
-  )
-  .catch(err => 
-    dispatch({
-      type: GET_PROFILES,
-      payload: null
-    })
-  )
-
-}
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
+};
 
 // Add Experience
 export const addExperience = (expData, history) => dispatch => {
--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -38,7 +38,8 @@ class ProfileItem extends Component {
             <ul className="list-group">
               {profile.skills.slice(0, 4).map((skill, index) => (
                 <li key={index} className="list-group-item">
-                  <i className="fas fa-check pr-1" />
+                  {/* <i className="fas fa-check pr-1" /> */}
+                  <i class="material-icons pr-1 mt-1">done</i>
                   {skill}
                 </li>
               ))}
